48e652922cb82769b2057f6a3ed929bad1d12517
MC-Kangan <97443307+MC-Kangan@users.noreply.github.com>
2022-02-09 21:28:02 +0000

    Exercise 4: Added master skill

--- interrupts.c
+++ interrupts.c
@@ -6,8 +6,8 @@
  * Note you also need to enable peripheral interrupts in the INTCON register to use CM1IE.
 ************************************/
 void Interrupts_init(void)
-{   //PIE2bits.C1IE=1; 	//enable comparator
-    //INTCONbits.GIEL=1;  // turn on peripheral interrupts
+{   PIE2bits.C1IE=1; 	//enable comparator
+    INTCONbits.GIEL=1;  // turn on peripheral interrupts
     PIE0bits.TMR0IE = 1;
     INTCONbits.GIE=1; 	//turn on interrupts globally
 	// turn on global interrupts, peripheral interrupts and the interrupt source 
@@ -28,5 +28,8 @@ void __interrupt(high_priority) HighISR()
     PIR0bits.TMR0IF = 0;  // clear the flag
 	}
 	//add your ISR code here i.e. check the flag, do something (i.e. toggle an LED), clear the flag...
+
+    if (PIR2bits.C1IF){ // if C1IF ==1                                       //check the interrupt source for the comparator; When surrounding light is dark, turn on LED; vice versa.
+        PIR2bits.C1IF = 0; }    
 }
 
--- main.c
+++ main.c
@@ -11,6 +11,7 @@
 #include "ADC.h"
 #include "interrupts.h"
 #include "timers.h"
+#include "comparator.h"
 
 #define _XTAL_FREQ 64000000 //note intrinsic _delay function is 62.5ns at 64,000,000Hz  
 
@@ -25,7 +26,7 @@ void main(void) {
     //	char buf[0]; // Define an array to store string
 
     unsigned char player_pos = 0xC0 + 7;        // position at the second row
-    unsigned char enemy_pos = 0XC0 + 18;  // position at the end of the second row
+    unsigned char enemy_pos = 0XC0 + 20;  // position at the end of the second row
     unsigned char enemy_pos2 = 0X80;  // position at the end of the first row
     unsigned char bullet1 = 0;
     unsigned char bullet2 = 0;
@@ -38,17 +39,10 @@ void main(void) {
     ANSELFbits.ANSELF3=0; //turn off analogue input on pin  
     
     while (1) {
-    //LCD_scroll();
-//        LCD_setline(1); // Set Line 1
-//        ADC_val = ADC_getval(); // Get ADC_value
-//        ADC2String(buf,ADC_val); // Create the string to be displayed
-//        LCD_sendstring(buf);
-//        __delay_ms(300);
-        //Sleep();
         if (T0CON0bits.T0OUT){
             Timer0_restart();
             second += 1;
-            if (--enemy_pos < 0xC0) {enemy_pos = 0xC0 + 18;}
+            if (--enemy_pos < 0xC0) {enemy_pos = 0xC0 + 20;}
             if (++enemy_pos2 > 0x80 + 15) {enemy_pos2 = 0x80;}
             if (second % 5 == 0){bullet1 = enemy_pos;} 
             if (second % 3 == 0){bullet2 = enemy_pos2;} 
@@ -60,15 +54,22 @@ void main(void) {
             player_pos = LCD_move(player_pos,enemy_pos,enemy_pos2,bullet1,bullet2);
             __delay_ms(200); // Delay is needed to improve the smoothness of the player's movement
         }
+        if (PORTFbits.RF3 == 0 && PORTFbits.RF2 == 0){
+            LCD_setline(1);
+            LCD_sendstring("((((  BOOM  ))))");               //display 'Game Over!!' in Line 1
+            LCD_setline(2);
+            LCD_sendstring("((((  BOOM  ))))");               //display 'Press RF2 to Go' in Line 2
+            __delay_ms(2000);
+        }
+                
         if (player_pos == enemy_pos || player_pos == enemy_pos2 || player_pos == bullet1 || player_pos == bullet2){
-            
             LCD_setline(1);
             LCD_sendstring("Game Over!!     ");               //display 'Game Over!!' in Line 1
             LCD_setline(2);
             LCD_sendstring("Press RF2 to Go");     //display 'Press RF2 to Go' in Line 2
             while (PORTFbits.RF2);
             __delay_ms(200); // Delay is needed to improve the smoothness of the player's movement
-            //break;
+            
         }
     }
 }
